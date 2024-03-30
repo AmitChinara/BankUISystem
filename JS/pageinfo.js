ff124256573568a480d910ed90cd7074a24a8f32
Amit Chinara <amitchinara@gmail.com>
2024-03-30 12:16:29 +0530

    Added functionality, to create account form

---
+++ JS/pageinfo.js
@@ -0,0 +1,13 @@
+const main_page_info = `<div class="option-container">
+                                <div class="options" id="option-1" onclick="createAccount()"><div class="text-container" id="text-1"><p>CREATE ACCOUNT</p></div></div>
+                                <div class="options" id="option-2" onclick="createAccountLogin()"><div class="text-container" id="text-2"><p>ACCOUNT LOGIN</p></div></div>
+                                <div class="options" id="option-3" onclick="createAccountDelete()"><div class="text-container" id="text-3"><p>DELETE ACCOUNT</p></div></div>
+                               </div>`;
+
+const create_account_form = `<div class="form-container">
+                                        
+                                   </div>`;
+
+const account_login_info= ``;
+
+const delete_account_form = ``;
--- JS/script.js
+++ JS/script.js
@@ -1,5 +1,35 @@
 const logo_container = document.getElementsByClassName("logo-container")[0];
+const main_data = document.getElementsByClassName("main-bottom")[0];
 const main_ip = `http://localhost:63342`;
-logo_container.addEventListener("click", () => {
-    window.location.href = `${main_ip}/BankUISystem/index.html`
-})
+logo_container.addEventListener("click", () => loadData(main_page_info))
+
+const loadData = (data) => main_data.innerHTML = data;
+
+const createAccount = () => {
+    let data = create_account_form;
+    loadData(data);
+}
+
+const createAccountLogin = () => {
+    let data = account_login_info;
+    loadData(data);
+}
+
+const createAccountDelete = () => {
+    let data = delete_account_form;
+    loadData(data);
+}
+
+const adjustAspectRatio = () => {
+    let form_container = document.getElementsByClassName('form-container')[0];
+    let width = form_container.offsetWidth;
+
+    if (width < 338.263)
+        form_container.style.aspectRatio = `1 / 1.2`;
+    else
+        form_container.style.aspectRatio = `1.75`;
+
+}
+window.addEventListener("resize", adjustAspectRatio);
+adjustAspectRatio();
+
